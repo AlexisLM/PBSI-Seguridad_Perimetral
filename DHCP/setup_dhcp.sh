e3062a932bb05fb5943890932cff88a88893e9a8
Alexis Brayan Lopez Matias <alexis-blm@ciencias.unam.mx>
2020-10-06 21:53:42 -0500

    Instalación y conf de DHCP

---
+++ DHCP/setup_dhcp.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#------------------------- Establecemos dirección IP estática
+IPADDR='192.168.20.20'
+NETINTERFACE='ens33'
+NETMASK='255.255.255.0'
+GATEWAY='192.168.20.2'
+DNS='8.8.8.8'
+
+# Set IP
+echo -e "\nGenerando /etc/network/interfaces..."
+echo "# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+auto ${NETINTERFACE}
+iface ${NETINTERFACE} inet static
+        address ${IPADDR}
+        netmask ${NETMASK}
+        gateway ${GATEWAY}
+        dns-nameservers ${DNS}
+
+" > /etc/network/interfaces
+
+echo "Reiniciando el servicio de red..."
+systemctl restart networking
+
+echo -e "IP ${IPADDR} establecida exitosamente!\n"
+
+#------------------------- Instalamos servicio de DHCP
+echo "Instalando y configurando servidor DHCP..."
+
+# Actualizamos bdd
+apt-get update
+
+# Instalamos el paquete para el dhcp
+apt-get install -y isc-dhcp-server
+
+sed -Ei 's/^#(DHCPDv4_CONF=.+)/\1/g' /etc/default/isc-dhcp-server
+sed -Ei 's/^(INTERFACESv4=)""/\1"ens33"/g' /etc/default/isc-dhcp-server
+
+# Agregamos subred para la vpn
+echo "
+## SubNet vpn
+subnet 192.168.20.0 netmask 255.255.255.0 {
+  authoritative;
+  range 192.168.20.32 192.168.20.63;
+  default-lease-time 3600;
+  max-lease-time 3600;
+  option subnet-mask 255.255.255.0;
+  option broadcast-address 192.168.20.255;
+  option routers 192.168.20.1;
+  option domain-name-servers 8.8.8.8;
+  option domain-name \"mdp-14t\";
+}
+" >> /etc/dhcp/dhcpd.conf
+
+# Habilitamos y reiniciamos servicio
+echo "Habilitando servicio de DHCP..."
+systemctl enable isc-dhcp-server.service
+echo "Iniciando servicio de DHCP..."
+systemctl restart isc-dhcp-server.service
+
+echo "Instalación y configuración finalizada!"
