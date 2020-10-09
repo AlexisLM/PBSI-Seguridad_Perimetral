b4d64f008187b1fd72924c596dba8ce2845acd01
Alexis Brayan Lopez Matias <alexis-blm@ciencias.unam.mx>
2020-10-09 02:59:08 -0500

    Se modifica setup del server

--- ldap/ldap_server.sh
+++ ldap/ldap_server.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+
+#------------------------- Configuramos la dirección IP estática
+IPADDR='192.168.20.130'
+NETINTERFACE='ens33'
+NETMASK='255.255.255.0'
+GATEWAY='192.168.20.2'
+DNS="192.168.20.150 ${GATEWAY}"
+DOMAIN='becarios.local'
+HOSTNAME="ldap.${DOMAIN}"
+
+echo -e "\nGenerando /etc/network/interfaces..."
+echo "# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+source /etc/network/interfaces.d/*
+# The loopback network interface
+auto lo
+iface lo inet loopback
+auto ${NETINTERFACE}
+iface ${NETINTERFACE} inet static
+        address ${IPADDR}
+        netmask ${NETMASK}
+        gateway ${GATEWAY}
+        dns-nameservers ${DNS}
+" > /etc/network/interfaces
+
+echo "Reiniciando el servicio de red..."
+systemctl restart networking
+
+echo -e "IP ${IPADDR} establecida exitosamente!\n"
+
+#------------------------- Configuramos el dns local
+echo "domain ${DOMAIN}
+search ${DOMAIN}
+nameserver 192.168.20.150
+nameserver ${GATEWAY}" > /etc/resolv.conf
+chattr +i /etc/resolv.conf
+
+#------------------------- Configuramos el nombre del host
+hostnamectl set-hostname "${HOSTNAME}"
+echo "${IPADDR} ${DOMAIN} ${HOSTNAME}" >> /etc/hosts
+
+#------------------------- Configuramos el servidor ldap
 apt -y install slapd ldap-utils ldapscripts
 dpkg-reconfigure slapd
 mkdir -p /etc/ssl/openldap/{private,certs,newcerts}
