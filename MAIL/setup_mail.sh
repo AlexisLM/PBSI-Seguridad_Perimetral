31f01b01e5ac79a5afe83e4fe3eea0f2b183fcf9
Alexis Brayan Lopez Matias <alexis-blm@ciencias.unam.mx>
2020-10-08 22:51:11 -0500

    Se actualiza setup

--- MAIL/setup_mail.sh
+++ MAIL/setup_mail.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-#------------------------- Establecemos dirección IP estática
-IPADDR='192.168.20.120'
+#------------------------- Configuramos la dirección IP estática
+IPADDR='192.168.20.110'
 NETINTERFACE='ens33'
 NETMASK='255.255.255.0'
 GATEWAY='192.168.20.2'
-DNS='8.8.8.8'
+DNS="192.168.20.150 ${GATEWAY}"
+DOMAIN='becarios.local'
+HOSTNAME="mail.${DOMAIN}"
 
-# Set IP
 echo -e "\nGenerando /etc/network/interfaces..."
 echo "# This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
-
 source /etc/network/interfaces.d/*
-
 # The loopback network interface
 auto lo
 iface lo inet loopback
-
 auto ${NETINTERFACE}
 iface ${NETINTERFACE} inet static
         address ${IPADDR}
         netmask ${NETMASK}
         gateway ${GATEWAY}
         dns-nameservers ${DNS}
-
 " > /etc/network/interfaces
 
 echo "Reiniciando el servicio de red..."
@@ -32,9 +29,15 @@ systemctl restart networking
 
 echo -e "IP ${IPADDR} establecida exitosamente!\n"
 
+#------------------------- Configuramos el dns local
+echo "domain ${DOMAIN}
+search ${DOMAIN}
+nameserver 192.168.20.150
+nameserver ${GATEWAY}" > /etc/resolv.conf
+
 #------------------------- Configuramos el nombre del host
-hostnamectl set-hostname mail.becarios.local
-echo "192.168.20.120 becarios.local mail.becarios.local" >> /etc/hosts
+hostnamectl set-hostname "${HOSTNAME}"
+echo "${IPADDR} ${DOMAIN} ${HOSTNAME}" >> /etc/hosts
 
 #------------------------- Instalamos servicio de MAIL
 # Actualizamos repositorios
